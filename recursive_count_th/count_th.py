d4ec7e600cb920c05b3c1624ae371221ee17f506
Greg Johnston <gcjohnston91@gmail.com>
2020-04-17 12:03:15 -0400

    Wrote and programmed out psuedo code but tests won't run. They loop for some reason.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,17 +5,18 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 
 
-def count_th(word, count=0):
-	print(word)
-	if len(word) < 2:
+def count_th(word, count=0):            # Moved count into params of func
+										# When it was in body, every loop reset it to 0
+
+	if len(word) < 2:                   # If the length is less than 2, it cannot contain 'th'
 		return count
-	elif word[:2] == 'th':
-		count += 1
-	return count_th(word[1:], count)
+	elif word[:2] == 'th':              # Check the first 2 chars of the string for 'th'
+		count += 1                      # If present, increment count by 1
+	return count_th(word[1:], count)    # Return the function and start and the 1st index of the word and return the count
 
 
-def begin(word):
-	return count_th(word)
+def begin(word):                        # Main function that just lets me pass in a string
+	return count_th(word)               # Returns the function with the string as a param
 
 
 print(begin('afdlkjasdfl;kthsadfkljdsalkthaskldfjdsklthasdklfjdsalkthsakldfjdslkthththth'))
